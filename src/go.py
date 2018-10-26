c08e747f2e1e9ad2512439b13255f760c4aa1310
mikebrgs <miguel.borges@hotmail.com>
2018-10-26 19:28:30 +0100

    Everything is developed but not tested.

--- src/go.py
+++ src/go.py
@@ -1,10 +1,16 @@
 # standard imports
 import sys
+import copy
 
 sys.path.insert(0, '/Users/mikebrgs/CurrentWork/tecnico/iasd/proj1/ext/aima-python/')
 import games
 
 PLAYERS = [1,2]
+EMPTY = 0
+WIN = 1
+DRAW = 0
+LOSS = -1
+INCOMPLETE = -1
 
 class State(object):
   """docstring for State"""
@@ -61,6 +67,19 @@ class State(object):
       item = self.board[crow][ccolumn]
     return item
 
+  def clone(self):
+    return copy.deepcopy(self)
+
+  # Where are the stones with colours in colours.
+  # colours should be a list
+  def stoneplaces(self, colours):
+    places = list()
+    for row in range(0, self.side):
+      for column in range(0, self.side):
+        if self.board[row][column] in colours:
+          places.append((row+1,column+1))
+    return places
+
   def __str__(self):
     string = ""
     for i in range(0,self.side):
@@ -76,114 +95,130 @@ class State(object):
     string += "Next player: " + str(self.turn) + "\n"
     return string
 
-class GameOver(object):
-  """docstring for GameOver"""
-  def __init__(self):
-    super(GameOver, self).__init__()
+class GameAnalytics(object):
+  """docstring for GameAnalytics"""
+  def __init__(self, state = None):
+    super(GameAnalytics, self).__init__()
+    # Maybe for later
+    self.state = None
 
-  def cluster(self):
-    for row in range(1, self.state.rows()+1):
-      for column in range(1, self.state.columns()+1):
+  def cluster(self,state):
+    clusters = list()
+    for row in range(1, state.rows()+1):
+      for column in range(1, state.columns()+1):
         # Skip empty spots
-        if self.state.getitem(row,column) == 0:
+        if state.getitem(row,column) == 0:
           continue
         id = (row,column)
         # Check if already associated to a cluster
-        flat_clusters = [stone for cluster in self.clusters for stone in cluster]
+        flat_clusters = [stone for cluster in clusters for stone in cluster]
         # for cluster in clusters:
         if id in flat_clusters:
           continue
         # If not, create a new cluster
         cluster = list()
-        cluster = self.next(cluster, row, column, self.state.getitem(row,column))
-        self.clusters.append(cluster)
-    return
+        cluster = self.next(state, cluster, row, column, state.getitem(row,column))
+        clusters.append(cluster)
+    return clusters
 
-  def next(self, cluster, row, column, stone_colour):
+  def next(self, state, cluster, row, column, stone_colour):
     stone_id = (row, column)
     cluster.append(stone_id)
-    stone_colour = self.state.getitem(row, column)
-    if (self.state.getitem(row+1, column) == stone_colour
+    stone_colour = state.getitem(row, column)
+    if (state.getitem(row+1, column) == stone_colour
       and not ((row+1,column) in cluster) ):
-      cluster = self.next(cluster, row+1, column, stone_colour)
-    if (self.state.getitem(row-1, column) == stone_colour
+      cluster = self.next(state, cluster, row+1, column, stone_colour)
+    if (state.getitem(row-1, column) == stone_colour
       and not ((row-1,column) in cluster) ):
-      cluster = self.next(cluster, row-1, column, stone_colour)
-    if (self.state.getitem(row, column+1) == stone_colour
+      cluster = self.next(state, cluster, row-1, column, stone_colour)
+    if (state.getitem(row, column+1) == stone_colour
       and not ((row,column+1) in cluster) ):
-      cluster = self.next(cluster, row, column+1, stone_colour)
-    if (self.state.getitem(row, column-1) == stone_colour
+      cluster = self.next(state, cluster, row, column+1, stone_colour)
+    if (state.getitem(row, column-1) == stone_colour
       and not ((row,column-1) in cluster) ):
-      cluster = self.next(cluster, row, column-1, stone_colour)
+      cluster = self.next(state, cluster, row, column-1, stone_colour)
     return cluster
 
-  def suicide(self, row, column, stone_colour):
-    stone_id = (row, column)
-    cluster = list()
-    cluster = self.next(cluster, row, column, stone_colour)
-    is_suicide = True
+  # Returns true if a play is suicidal or false otherwise
+  def suicide(self, state, row, column, stone_colour):
+    suicide_state = state.clone()
+    suicide_state.play(stone_colour,row,column)
+    suicide_cluster = self.next(state, list(), row, column, stone_colour)
+    return self.surrounded(state, suicide_cluster)
+
+  # Checks the surroundings of a cluster and returns True if it's surronded
+  # or false otherwise
+  def surrounded(self, state, cluster):
     for stone in cluster:
-      if (not (self.state.getitem(stone[0]+1,stone[1]) in PLAYERS)
-        and not (self.state.getitem(stone[0]+1,stone[1]) is None)):
-        is_suicide = False
-        break
-      if (not (self.state.getitem(stone[0]-1,stone[1]) in PLAYERS)
-        and not (self.state.getitem(stone[0]-1,stone[1]) is None)):
-        is_suicide = False
-        break
-      if (not (self.state.getitem(stone[0],stone[1]+1) in PLAYERS)
-        and not (self.state.getitem(stone[0],stone[1]+1) is None)):
-        is_suicide = False
-        break
-      if (not (self.state.getitem(stone[0],stone[1]-1) in PLAYERS)
-        and not (self.state.getitem(stone[0],stone[1]-1) is None)):
-        is_suicide = False
-        break
-    if is_suicide:
-      return True
-
-  def checkborders(self):
-    for cluster in self.clusters:
-      is_gameover = True
-      for stone in cluster:
-        if (not (self.state.getitem(stone[0]+1,stone[1]) in PLAYERS)
-          and not (self.state.getitem(stone[0]+1,stone[1]) is None)):
-          is_gameover = False
-          break
-        if (not (self.state.getitem(stone[0]-1,stone[1]) in PLAYERS)
-          and not (self.state.getitem(stone[0]-1,stone[1]) is None)):
-          is_gameover = False
-          break
-        if (not (self.state.getitem(stone[0],stone[1]+1) in PLAYERS)
-          and not (self.state.getitem(stone[0],stone[1]+1) is None)):
-          is_gameover = False
-          break
-        if (not (self.state.getitem(stone[0],stone[1]-1) in PLAYERS)
-          and not (self.state.getitem(stone[0],stone[1]-1) is None)):
-          is_gameover = False
-          break
-      if is_gameover:
-        return self.state.getitem(stone[0],stone[1])
-    full = True
-    for row in range(1,self.state.rows()+1):
-      for column in range(1,self.state.columns()+1):
-        if self.state.getitem(row,column) == 0:
-          return -1
-    return 0
-
-  def condition(self, state):
-    self.state = state
-    self.clusters = list()
-    self.cluster()
-    if self.checkborders() == -1:
-      return False
+      if (not (state.getitem(stone[0]+1,stone[1]) in PLAYERS)
+        and not (state.getitem(stone[0]+1,stone[1]) is None)):
+        return False
+      if (not (state.getitem(stone[0]-1,stone[1]) in PLAYERS)
+        and not (state.getitem(stone[0]-1,stone[1]) is None)):
+        return False
+      if (not (state.getitem(stone[0],stone[1]+1) in PLAYERS)
+        and not (state.getitem(stone[0],stone[1]+1) is None)):
+        return False
+      if (not (state.getitem(stone[0],stone[1]-1) in PLAYERS)
+        and not (state.getitem(stone[0],stone[1]-1) is None)):
+        return False
     return True
 
-  def winner(self, state):
-    self.state = state
-    self.clusters = list()
-    self.cluster()
-    return self.checkborders()
+  # Returns the player who won the game, 0 for a draw or -1 for a game
+  # that is not over
+  def condition(self, state):
+    # Checks if the clusters are surrounded
+    clusters = self.cluster(state)
+    for cluster in clusters:
+      if self.surrounded(state,cluster):
+        stone = cluster[0]
+        return state.getitem(stone[0],stone[1])
+    # Check if the board is full -- in this case we are not able to tell
+    # who one. This is a bad board, shouldn't happen
+    if (len(state.stoneplaces(PLAYERS)) >=
+      state.rows() * state.columns()):
+      return DRAW
+    # Checks for suicidal moves
+    empty_points = state.stoneplaces([EMPTY])
+    for blank in empty_points:
+      if not self.suicide(state,blank[0],blank[1],state.next()):
+        return INCOMPLETE
+    return DRAW
+
+  # Returns the points of freedom of a specific cluster
+  def freedom(self, state, cluster):
+    freedom_score = 0
+    freedom_list = list()
+    for stone in cluster:
+      if (state.getitem(stone[0]+1,stone[1]) == EMPTY
+        and not ((stone[0]+1,stone[1]) in freedom_list)):
+        freedom_score += 1
+      if (state.getitem(stone[0]-1,stone[1]) == EMPTY
+        and not ((stone[0]-1,stone[1]) in freedom_list)):
+        freedom_score += 1
+      if (state.getitem(stone[0],stone[1]+1) == EMPTY
+        and not ((stone[0],stone[1]+1) in freedom_list)):
+        freedom_score += 1
+      if (state.getitem(stone[0],stone[1]-1) == EMPTY
+        and not ((stone[0],stone[1]-1) in freedom_list)):
+        freedom_score += 1
+    return freedom_score
+
+
+  def score(self, state, player):
+    clusters = self.cluster(state)
+    freedoms = dict()
+    for plr in PLAYERS:
+      freedoms[plr] = list()
+    # colours = list()
+    for cluster in clusters:
+      stone = cluster[0]
+      colour = state.getitem(stone[0],stone[1])
+      freedoms[colour].append(self.freedom(state,cluster))
+    this_score = float(max(freedoms[player]))
+    other_score = float(max(freedoms[freedoms.index(player)-1]))
+    return (other_score-this_score)/(other_score+this_score)
+
 
 class Game(object):
   """docstring for Game"""
@@ -194,26 +229,34 @@ class Game(object):
     return s.next()
     
   def terminal_test(self, s):
-    gameover = GameOver()
-    return gameover.condition(s)
+    analytics = GameAnalytics()
+    # analytics.setstate(s)
+    condition = analytics.condition(s)
+    if condition == INCOMPLETE:
+      return False
+    return True
 
   def utility(self, s, p):
-    gameover = GameOver()
-    winner = gameover.winner(s)
-    if winner == p:
+    analytics = GameAnalytics()
+    # analytics.setstate(s)
+    condition = analytics.condition(s)
+    if condition == p:
       return 1
-    elif winner == 0:
+    elif condition == DRAW:
       return 0
-    elif winner in PLAYERS:
+    elif condition in PLAYERS:
       return -1
-    # Real utility function
+    return analytics.score(s,p)
     
 
   def actions(self, s):
+    analytics = GameAnalytics()
+    # analytics.setstate(s)
     actions = list()
     for row in range(1, s.rows()+1):
       for column in range(1, s.columns()+1):
-        if s.getitem(row,column) == 0:
+        if (s.getitem(row,column) == 0
+          and analytics.suicide(s,row,column,s.next())):
           actions.append((s.next(), row, column))
     return actions
 
@@ -241,15 +284,17 @@ class Game(object):
     return state
 
 def main(argv):
-  file = open("/Users/mikebrgs/CurrentWork/tecnico/iasd/proj1/data/data4.txt", "r")
+  file = open("/Users/mikebrgs/CurrentWork/tecnico/iasd/proj1/data/data6.txt", "r")
   game = Game()
   state = game.load_board(file)
-  print(state)
+  # cloned_state = game.result(state.clone(), (1,1,1))
+  # print(state)
+  # print(cloned_state)
+  # print(state)
   print(game.terminal_test(state))
   # print(games.alphabeta_search(state, game))
   # print(state)
   # print(game.actions(state))
-  # state = game.result(state, (1,1,1))
   # print(state)
   # state = game.result(state, (2,2,1))
   # print(state)
